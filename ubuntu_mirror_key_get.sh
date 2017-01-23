edab3c7f93cd671d169eae9069c9b12a583d7073
konono <kono@kononet.net>
2017-01-23 11:04:22 +0900

    get ubuntu mirror key

---
+++ ubuntu_mirror_key_get.sh
@@ -0,0 +1,4 @@
+gpg --no-default-keyring --keyring trustedkeys.gpg --keyserver keys.gnupg.net --recv-keys 40976EAF437D05B5 3B4FE6ACC0B21F32
+gpg --no-default-keyring --keyring trustedkeys.gpg --keyserver keys.gnupg.net --recv-keys 5EDB1B62EC4926EA
+gpg --no-default-keyring --keyring trustedkeys.gpg --keyserver keys.gnupg.net --recv-keys 376A290EC8068B11
+gpg --no-default-keyring --keyring trustedkeys.gpg --keyserver keys.gnupg.net --recv-keys 04E7FDC5684D4A1C
