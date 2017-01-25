094d72b346d62cac2044e77a2a9cacd147aae32f
konono <kono@kononet.net>
2017-01-25 19:27:21 +0900

    fix bug

--- publish_xenial_ubuntu_snapshot.sh
+++ publish_xenial_ubuntu_snapshot.sh
@@ -4,6 +4,7 @@ for distribution in `aptly mirror list -raw|xargs -I%  aptly mirror show %|grep 
 do
         if [ distribution = xenial-security ]; then
                 sudo aptly publish snapshot -architectures="amd64,source" -component=,,, -gpg-key="300F9C58" -distribution="$l" security_ubuntu_com_${distribution}_main-${date} security_ubuntu_com_${distribution}_restricted-${date} security_ubuntu_com_${distribution}_universe-${date} security_ubuntu_com_${distribution}_multiverse-${date} ubuntu
-        fi
-        sudo aptly publish snapshot -architectures="amd64,source" -component=,,, -gpg-key="300F9C58" -distribution="$l" jp_archive_ubuntu_com_${distribution}_main-${date} jp_archive_ubuntu_com_${distribution}_restricted-${date} jp_archive_ubuntu_com_${distribution}_universe-${date} jp_archive_ubuntu_com_${distribution}_multiverse-${date} ubuntu
+        else
+        	sudo aptly publish snapshot -architectures="amd64,source" -component=,,, -gpg-key="300F9C58" -distribution="$l" jp_archive_ubuntu_com_${distribution}_main-${date} jp_archive_ubuntu_com_${distribution}_restricted-${date} jp_archive_ubuntu_com_${distribution}_universe-${date} jp_archive_ubuntu_com_${distribution}_multiverse-${date} ubuntu
+	fi
 done
